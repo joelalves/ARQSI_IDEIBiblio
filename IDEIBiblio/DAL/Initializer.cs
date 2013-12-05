ee8e3deb39ab8d609aec4afe9146fcdafde177be
joelalves <joel.17.alves@gmail.com>
2013-12-05 00:11:23 +0000

    add project

---
+++ IDEIBiblio/DAL/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using IDEIBiblio.Models;
+
+namespace IDEIBiblio.DAL
+{
+    public class Initializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<IDEIBiblioContext>
+    {
+    }
+}
---
+++ IDEIBiblio/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDEIBiblio.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        public String nome { get; set; }
+
+        
+    
+    }
+}
